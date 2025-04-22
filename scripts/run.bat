974a708d29c0ee70f673debc3c5f29bbca52436c
Creray <creray.default@gmail.com>
2025-04-22 23:04:42 +0700

    Add individual .for file compilation

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,11 +1,24 @@
 @echo off
 setlocal
 
-for /f "delims=" %%A in ('cd') do (
-    set PROJECT_NAME=%%~nxA
+if [%1]==[] (
+    echo Error: No file specified
+    echo Usage: %~nx0 filename.for
+    exit /b 1
+)
+if not "%~x1"==".for" (
+    echo Error: File must have .for extension
+    exit /b 1
+)
+if not exist %1 (
+    echo Error: File not found: %1
+    exit /b 1
+)
+for %%A in (%~1) do (
+    set filename=%%~nA
 )
 
-if not exist out\%PROJECT_NAME%.exe (
+if not exist out\%filename%.exe (
     echo Error: Nothing to run
     exit /b 1
 )
--- src/example.for
+++ src/example.for
@@ -1,4 +1,8 @@
       program example
         implicit none
-        print *, 'Hello, World'
+        integer n
+        open(10, file='test.txt', status='old')
+        read(10, *) n
+        close(10)
+        print *, 'The number in the test.txt is :', n
       end
